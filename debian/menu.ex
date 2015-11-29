bc1c173cf7650f0fc4a9e57b54a9b96083ce31b0
Gasol Wu <gasol.wu@gmail.com>
2015-11-29 06:20:14 +0000

    Remove *.ex *.EX

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(php-yaml):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="php-yaml" command="/usr/bin/php-yaml"
--- debian/php-yaml.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the php-yaml package
-#
-0 4	* * *	root	[ -x /usr/bin/php-yaml_maintenance ] && /usr/bin/php-yaml_maintenance
